413e8859820adb2fd4be2f90ae0c1d8ee7ede44f
lee sang wook <sanguk9907>
2024-02-22 01:01:10 +0900

    first commit

---
+++ myapp/myapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myapp
+//
+//  Created by 이상욱 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
