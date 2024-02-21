9c3682df4b903c2a02c44d45f12eb461212708b7
lee sang wook <sanguk9907>
2024-02-22 01:01:24 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <MainScreen/>
+    </Provider>
   );
 }
 
---
+++ components/TodoItem.js
@@ -0,0 +1,81 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import CheckBoxUnchecked from '../assets/checkbox-unchecked.svg'
+import CheckBoxChecked from '../assets/checked-checked.svg'
+import DeleteIcon from '../assets/delete.svg'
+import { useDispatch } from 'react-redux'
+import { deleteTodo, updateTodo } from '../redux/slice/TodoSlice'
+
+const TodoItem = (props) => {
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.itemContainer}>
+      <Pressable 
+      hitSlop={10} 
+      style={styles.itemCheckBox}
+      onPress={()=>dispatch(updateTodo(props.id)) }
+      >
+        {props.state === 'todo' ?
+        <CheckBoxUnchecked style={styles.unCheckedIcon}/>
+        :
+        <CheckBoxChecked style={styles.CheckedIcon}/>
+        }
+      </Pressable>
+      <Text style={[styles.itemText, 
+        props.state === 'done' ? styles.itemTextChecked : '']}>{props.text}</Text>
+      <Pressable style={[styles.deleteButton,
+        props.state === 'done' && styles.deleteButtonDone]} hitSlop={10}
+        onPress={()=>dispatch(deleteTodo(props.id))}
+        >
+        <DeleteIcon style={styles.deleteIcon }/>
+      </Pressable>
+    </View>
+  )
+}
+
+export default TodoItem
+
+const styles = StyleSheet.create({
+  itemContainer:{
+    flexDirection:"row",
+    alignItems:"center",
+    paddingTop:10,
+    paddingBottom:15,
+    paddingHorizontal:15,
+    backgroundColor:"#f7f8fa"
+  },
+  itemCheckBox:{
+    justifyContent:"center",
+    alignItems:"center",
+    width:20,
+    height:20,
+    marginRight:13,
+    borderRadius:6
+  },
+  CheckedIcon:{
+    shadowColor:"#000",
+    shadowRadius:8,
+    shadowOffset:{
+      width:0,
+      height:4
+    }
+  },
+  itemText:{
+    marginRight:"auto",
+    paddingRight:25,
+    fontSize:15,
+    lineHeight:20,
+    color:"#737373"
+  },
+  itemTextChecked:{
+    opacity:0.3,
+    textDecorationLine:"underline line-through"
+  },
+  deleteButton:{
+    opacity:0.8
+  },
+  deleteButtonDone:{
+    opacity:0.3
+  } 
+})
